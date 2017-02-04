1073a43c97aff8c7a0230f677b23cfee0559da5e
Kendra Lyndon <kendralyndon@gmail.com>
2017-02-03 22:42:14 -0700

    add content

--- public/services/CategoryService.js
+++ public/services/CategoryService.js
@@ -4,32 +4,32 @@ app.factory('CategoryService', function(){
       {
         type: 'Coffee',
         photo: '../assets/img/Coffee.jpg',
-        description: 'For optimum freshness, coffee should be stored within the relative humidity range of 50% to 65% and within the temperature range of 33F to 68F (0.6C to 20C)'
+        description: 'The optimum temperature for storing roasted coffee ranges from 42°F to 77°F, with the ideal level of relative humidity between 50% and 65%.'
       },
       {
         type: 'Art',
         photo: '../assets/img/Art.jpg',
-        description: ''
+        description: 'The ideal temperature for storing art is between 66°F to 74oF, with the level of relative humidity ranging from 42% to 58%.'
       },
       {
         type: 'Cigars',
         photo: '../assets/img/Cigars.jpg',
-        description: ''
+        description: 'The optimum temperature for storing cigars is 68°F, with the ideal level of relative humidity between 65% and 75%.    To avoid unwanted dampening or drying out, a certain temperature/humidity ratio should be respected. Each decrease (or increase) of 1.8° Fahrenheit in temperature must be balanced out by the increase (or decrease) of 3.0 % in relative humidity (in the ratio of 1.8°F / 3.0 %).'
       },
       {
         type: 'Documents',
         photo: '../assets/img/Documents.jpg',
-        description: ''
+        description: 'The optimum temperature for storing documents is less than 70°F, although it can range from 41°F to 77°F, with ideal levels of humidity generally between 30% and 50% (the lower end of this range is preferable).'
       },
       {
         type: 'Fruits & Vegetables',
         photo: '../assets/img/Fruits-Vegetables.jpg',
-        description: ''
+        description: 'Optimum temperatures for storing fresh fruits and citrus range from 32°F to 61°F, depending on the specific item, with the ideal level of relative humidity between 80% and 90%.  In the case of dried fruit, the optimum temperature ranges from 32°F to 77°F with the ideal level of relative humidity between 50% and 70%.  The optimum temperature for storing vegetables ranges from 32°F to 58°F, depending on the specific item, with the ideal level of relative humidity between 80% and 95%.'
       },
       {
         type: 'Herbs',
         photo: '../assets/img/Herbs.jpg',
-        description: ''
+        description: 'The optimum temperature for storing herbs ranges from 32°F to 61°F, depending on the specific item, with the ideal level of relative humidity between 80% and 90%.'
       },
       {
         type: 'Organ Transport',
@@ -37,24 +37,24 @@ app.factory('CategoryService', function(){
         description: ''
       },
       {
-        type: 'Instruments',
+        type: 'String Instruments',
         photo: '../assets/img/Instruments.jpg',
-        description: ''
+        description: 'The optimum temperature for storing string instruments ranges from 65°F to 75°F, with the ideal level of relative humidity between 45% and 65%.'
       },
       {
         type: 'Medicine & Supplements',
         photo: '../assets/img/Medicine-Supplements.jpg',
-        description: ''
+        description: 'Optimum temperatures for medicine & supplements range from 58°F to 86°F, depending on the specific item, with the ideal level of relative humidity at or below 60%.'
       },
       {
         type: 'Seeds',
         photo: '../assets/img/Seeds.jpg',
-        description: ''
+        description: 'Seeds are unique in their long-term storage requirements.  They should be stored in a temperature and humidity controlled environment which the sum of the temperature (in Fahrenheit) plus the relative humidity (in percent) does not exceed 100.  Thus, if seeds are stored at a temperature of 70oF, the humidity should not exceed 30%.  Seeds should never be frozen unless the moisture content is below 1%.'
       },
       {
         type: 'Spices',
         photo: '../assets/img/Spices.jpg',
-        description: ''
+        description: 'The optimum temperature for storing spices ranges from 41°F to 77°F, with the ideal level of relative humidity between 50% and 75%, depending on the specific spice being stored.'
       },
       {
         type: 'Sports Equipment',
@@ -64,17 +64,17 @@ app.factory('CategoryService', function(){
       {
         type: 'Teas',
         photo: '../assets/img/Teas.jpg',
-        description: ''
+        description: 'The optimum temperature for storing teas ranges from 41°F to 77°F, with the ideal levels of relative humidity between 50% and 60%.'
       },
       {
-        type: 'Truffles',
+        type: 'Truffles and Mushrooms',
         photo: '../assets/img/Truffles.jpg',
-        description: ''
+        description: 'The optimum temperature for storing truffles and mushrooms ranges from 32°F to 36°F, with the ideal level of relative humidity at 95%.'
       },
       {
         type: 'Wine',
         photo: '../assets/img/Wine.jpg',
-        description: ''
+        description: 'The optimum temperature for storing wine ranges from 45°F to 60°F, depending upon the type, with the ideal level of relative humidity between 70% to 90%.  The typical wine refrigerator controls the temperature, but does not address humidity.  The result is the drying of the cork over time, allowing bacteria to reach, and spoil, the contents of the bottle.'
       }
     ]
   }
--- public/services/TeamService.js
+++ public/services/TeamService.js
@@ -4,27 +4,27 @@ app.factory('TeamService', function(){
       {
         name: 'Rick Bernheim',
         title: 'Founder and CEO',
-        photo: '../assets/img/placeholder.png',
+        photo: '../assets/img/Rick-Bernheim.jpg',
         accomplishments: [
           'Serial high-tech entrepreneur, 11 startup/early-stage companies',
           'Led businesses to $125M',
           '5 Granted and 11 in-process patents'
         ]
       },
-      // {
-        // name: 'Dr. Ray Gottesfeld, M.D.',
-        // title: 'VP QA and Clinical Affairs',
-        // photo: '../assets/img/placeholder.png',
-        // accomplishments: null
-        // [
-        //   'Serial entrepreneur, 6 startup/early-stage companies',
-        //   'Chief Medical Officer of $300M Division'
-        // ]
-      // },
+      {
+        name: 'Dr. Ray Gottesfeld, M.D.',
+        title: 'VP QA and Clinical Affairs',
+        photo: '../assets/img/Ray-Gottesfeld.jpg',
+        accomplishments: 
+        [
+          'Serial entrepreneur, 6 startup/early-stage companies',
+          'Chief Medical Officer of $300M Division'
+        ]
+      },
       {
         name: 'Frieda Sands',
         title: 'VP Marcom Secretary and Treasurer',
-        photo: '../assets/img/placeholder.png',
+        photo: '../assets/img/Frieda-Sands.jpg',
         accomplishments: [
           'Serial entrepreneur, 5 startup/early-stage companies',
           'Former director and board member of various non-profits'
@@ -33,7 +33,7 @@ app.factory('TeamService', function(){
       {
         name: 'John deLassus',
         title: 'VP Engineering and Operations',
-        photo: '../assets/img/placeholder.png',
+        photo: '../assets/img/John-deLassus.png',
         accomplishments: [
           'Serial high-tech entrepreneur, 7 startup/early-stage companies',
           '3 Granted patents'
