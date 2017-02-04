17f4fbf7f54be48e675632283b2c1b60d8c7d994
Kendra Lyndon <kendralyndon@gmail.com>
2017-02-03 22:49:22 -0700

    update faqs

--- public/services/FAQService.js
+++ public/services/FAQService.js
@@ -2,56 +2,58 @@ app.factory('FAQService', function(){
   return {
     questions : [
       {
-        question: "How much product does it hold?",
-        answer: "The BioSafe Home unit holds 4 separate 3 ounce (by volume) containers, for a total of ½ ounce (by weight) of organic material.  Larger units are available"
+        question: "How much product does the Biosafe Home hold?",
+        answer: "The BioSafe Home unit holds 4 separate 3-ounce (by volume) containers, each holding 1/8 ounce (by weight) of organic material.  Altogether, it holds a total of ½ ounce by weight.  Other storage configurations for the BioSafe Home unit are available.  Larger units are also available, including rectangular units that hold up to a pound of organic material, are battery operated, and may be stored overnight without external power."
       },
       {
         question: "Does it affect the taste?",
-        answer: "No, the BioSafe does not affect the taste of any organic material."
+        answer: "No, the BioSafe Home does not affect the taste of any organic material."
       },
       {
         question: "I live in a dry area.  Will it increase the humidity until it is at the proper level?",
-        answer: "The BioSafe is capable of raising the humidity of the storage chamber to the desired level in any environment with an average relative humidity of at least 15%.  This covers most of the cities in North America.  For environments with an average relative humidity of less than 15%, an optional humidity boost system is available."
+        answer: "In any area where the outside humidity is at least 20% (almost all North American cities), the BioSafe Home is capable of raising the relative humidity of the storage chamber as high as 90%.    For environments with an average relative humidity of less than 20%, an optional “humidity-boost” system is available."
       },
       {
-        question: "I live in a humid area.  Will it decrease the humidity until it is at the proper level?",
-        answer: "Yes, the BioSafe is capable of lowering the humidity of the storage chamber to the desired level in any environment with an average relative humidity of less than 95%."
+        question: "I live in a humid area.  Will it dry my products to the proper level?",
+        answer: "Yes, the BioSafe Home is capable of lowering the humidity of the storage chamber to the desired level in any area with a relative humidity of up to 95%."
       },
       {
         question: "I live in a humid area.  Why do I need a BioSafe to store organic material?",
-        answer: "Humidity over the desired level can create conditions of mold growth.  Consumption of moldy organic material is particularly problematic, and at minimum, can cause severe headaches."
+        answer: "Too much humidity is a set-up for overgrowth by molds and fungus, which can distort flavor, smell awful, and produce toxins."
       },
       {
         question: "I live in an area with high temperature.  How can the BioSafe help me?",
-        answer: "Moderate to high temperature, especially in conjunction with relative humidity exceeding 65%, sets up the ideal conditions for mold growth in organic material.  To combat this, the BioSafe is available with an optional temperature control module."
+        answer: "Moderate to high temperature, especially in conjunction with high relative humidity (over 65%,) contributes to growth of fungus and mold in organic material.  To combat this, the BioSafe is available with an optional temperature control module."
       },
       {
         question: "Does the BioSafe make noise?",
-        answer: "The BioSafe contains a whisper quiet air mover.  It is possible to hear the air moving, but only if you are within a few inches of the unit."
+        answer: "The BioSafe Home contains a whisper-quiet air mover which runs from time to time.  It makes less noise than a laptop computer fan.  For most people, it is inaudible except within a short distance of the unit."
       },
       {
         question: "How much power does the BioSafe use?",
-        answer: "The BioSafe Home uses less than 5 watts of power and plugs into a standard 1A (or more) cell phone charger."
+        answer: "The BioSafe Home uses less than 5 watts of power and plugs into a standard 1 Amp (or more) micro-USB cell phone charger."
       },
       {
-        question: "What are the dimensions?",
-        answer: "The BioSafe Home is 9” tall and 5” wide."
+        question: "How big is the unit?",
+        answer: "The BioSafe Home is cylindrical in shape.  Excluding the protrusions for the lid hinge and latch, the unit is 8-1/4” tall, with a 5” diameter."
       },
       {
-        question: "What is the warranty period?",
-        answer: "The BioSafe Home is warranted for 1 year against defects in material or workmanship.  An extended warranty program is also available.  Contact CFS for additional information."
+        question: "How long is the warranty?",
+        answer: "The BioSafe Home is warranted for 1 year against defects in material or workmanship.  An extended warranty is also available."
       },
       {
         question: "Is it available in different colors?",
-        answer: "The standard unit has a stainless steel body with a black base and black lid.  Optional colors for the base and lid are available."
+        answer: "Yes. The standard unit has a stainless steel body with a black base and black lid. Optional colors for the base and lid are available."
       },
       {
         question: "What do the different colored lights mean?",
-        answer: "The BioSafe Home has a single indicator that changes color to indicate the status of the storage chamber.  Green indicates the system is increasing the humidity of the storage chamber.  Red indicates the system is decreasing the humidity, and blue indicates that the storage chamber is at the target humidity.  One of these three colors is always displayed when the unit is powered."
+        answer: `The BioSafe Home has a single indicator that changes color to indicate the status of the storage chamber. Green mean the system is raising the humidity in the storage chamber.`+
+                `Red means the system is decreasing the humidity, and blue indicates that the storage chamber has reached, and is being maintained, at the target humidity. One of these three colors is always displayed when the unit is powered.`+
+                `A separate similar indicator displays cooling and heating operations in temperature-controlled units.`
       },
       {
         question: "I see how the BioSafe can keep my organics at the proper humidity and temperature, but can the BioSafe keep my organics secure?",
-        answer: "The BioSafe Home comes with a built-in combination lock on the cover, which can be set to any 4 digit sequence you desire."
+        answer: "The BioSafe Home comes with a built-in combination lock on the cover, which can be set to any 4 digit sequence you prefer.  The food-grade silicon seal also keeps all but the most pungent biologics odor-free."
       }
     ]
   }
