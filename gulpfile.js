edede806a7fbbf0ba758c3735d23abaf9459bc1a
Kendra Lyndon <kendralyndon@gmail.com>
2017-02-05 12:04:53 -0700

    add more responsive styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 var mainBowerFiles = require('main-bower-files');
 
-gulp.task('default', ['compress-src', 'compress-bower', 'minify-css']);
+gulp.task('default', ['compress-src', 'compress-bower', 'clean-css']);
 
 gulp.task('compress-bower', function () {
   return gulp.src(mainBowerFiles(), { base: '/bower_components' })
@@ -15,3 +16,9 @@ gulp.task('compress-src', function () {
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/build/scripts'));
 });
+
+gulp.task('clean-css', function () {
+  return gulp.src('public/assets/stylesheets/input.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('public/build/style'));
+})
