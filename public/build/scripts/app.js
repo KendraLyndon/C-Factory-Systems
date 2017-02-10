3de18cb086810324f0de515a9145c3b677f72884
Kendra Lyndon <kendralyndon@gmail.com>
2017-02-09 18:16:02 -0700

    update about section

--- public/build/scripts/app.js
+++ public/build/scripts/app.js
@@ -247,8 +247,8 @@ app.factory('TeamService', function(){
     teamMembers : [
       {
         name: 'Rick Bernheim',
-        title: 'Founder and CEO',
-        photo: '../assets/img/Rick-Bernheim.jpg',
+        title: 'President and CEO',
+        photo: '../assets/img/Rick-Bernheim-1.jpg',
         accomplishments: [
           'Serial high-tech entrepreneur, 11 startup/early-stage companies',
           'Led businesses to $125M',
@@ -256,22 +256,22 @@ app.factory('TeamService', function(){
         ]
       },
       {
-        name: 'Dr. Ray Gottesfeld, M.D.',
+        name: 'Frieda Sands',
+        title: 'VP Marcom Secretary and Treasurer',
+        photo: '../assets/img/Frieda-Sands-1.jpg',
+        accomplishments: [
+          'Serial entrepreneur, 5 startup/early-stage companies',
+          'Former director and board member of various non-profits'
+        ]
+      },
+      {
+        name: 'Ray Gottesfeld, M.D.',
         title: 'VP QA and Clinical Affairs',
-        photo: '../assets/img/Ray-Gottesfeld.jpg',
+        photo: '../assets/img/Ray-Gottesfeld-1.jpg',
         accomplishments:
         [
           'Serial entrepreneur, 6 startup/early-stage companies',
           'Chief Medical Officer of $300M Division'
-        ]
-      },
-      {
-        name: 'Frieda Sands',
-        title: 'VP Marcom Secretary and Treasurer',
-        photo: '../assets/img/Frieda-Sands.jpg',
-        accomplishments: [
-          'Serial entrepreneur, 5 startup/early-stage companies',
-          'Former director and board member of various non-profits'
         ]
       }
     ]
